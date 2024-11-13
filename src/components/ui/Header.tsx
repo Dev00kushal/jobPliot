2e2011e2e9af911c2781f3c922dc9ceb7c193001
Dev00kushal <devkushal00@gmail.com>
2024-11-13 16:08:01 +0545

    ✅ ✨ Add language selection to Header component; include country flag icons and update dependencies

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,14 +1,26 @@
 "use client";
-import BriefcaseLogo from "@/assets/briefcase.svg";
 import { PhoneCall } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./select";
+
 const links = [
   { name: "Home", href: "/" },
   { name: "Find Job", href: "/job" },
   { name: "Employers", href: "/find-a-employee" },
   { name: "Pricing Plans", href: "/find-a-pricing-plan" },
   { name: "Custom Supports", href: "/find-a-support" },
+];
+
+const countries = [
+  { code: "EN", name: "English" },
+  { code: "NP", name: "Nepal" },
 ];
 
 export default function Header() {
@@ -28,12 +40,27 @@ export default function Header() {
             }`}
           >
             {item.name}
-          </Link> 
+          </Link>
         ))}
-         <div className="flex items-center ml-auto">
-        <PhoneCall  className="scale-75"/> 
-         <span className=  "text-customNavtext ml-2">+1-202-555-0178</span>
 
+        <div className="flex items-center ml-auto gap-1">
+          <PhoneCall className="scale-75" />
+          <span className="text-customNavtext ml-2">+1-202-555-0178</span>
+
+          <Select>
+            <SelectTrigger className="w-[100px]">
+              <SelectValue placeholder="English" />
+            </SelectTrigger>
+            <SelectContent>
+              {countries.map((country) => {
+                return (
+                  <SelectItem key={country.code} value={country.code}>
+                    <div className="flex items-center">{country.name}</div>
+                  </SelectItem>
+                );
+              })}
+            </SelectContent>
+          </Select>
         </div>
       </nav>
 
@@ -41,12 +68,15 @@ export default function Header() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
           <div className="flex justify-between items-center">
             <Link href="#" className="flex items-center space-x-2">
-              <BriefcaseLogo/>
               <span className="text-xl font-semibold">Jobpilot</span>
             </Link>
             <div className="flex items-center space-x-4">
-              <button className="text-customBlue border p-4 rounded-md border-customBorderLight">Sign In</button>
-              <button className="bg-customBlue p-4 text-white rounded-md">Post A Jobs</button>
+              <button className="text-customBlue border p-4 rounded-md border-customBorderLight">
+                Sign In
+              </button>
+              <button className="bg-customBlue p-4 text-white rounded-md">
+                Post A Jobs
+              </button>
             </div>
           </div>
         </div>
