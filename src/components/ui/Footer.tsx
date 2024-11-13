4d7a43ff9e46972c935c2918cf433d146af90154
Dev00kushal <devkushal00@gmail.com>
2024-11-13 10:31:02 +0545

    ✨ Refactor Header component; move to UI folder, add phone contact, and enhance styles

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Facebook, Instagram, Twitter, Youtube } from 'lucide-react'
 import Link from "next/link"
 
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,7 +1,8 @@
 "use client";
+import BriefcaseLogo from "@/assets/briefcase.svg";
+import { PhoneCall } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import MyLogo from "../assets/briefcase.svg";
 const links = [
   { name: "Home", href: "/" },
   { name: "Find Job", href: "/job" },
@@ -27,15 +28,20 @@ export default function Header() {
             }`}
           >
             {item.name}
-          </Link>
+          </Link> 
         ))}
+         <div className="flex items-center ml-auto">
+        <PhoneCall  className="scale-75"/> 
+         <span className=  "text-customNavtext ml-2">+1-202-555-0178</span>
+
+        </div>
       </nav>
 
       <header className="border-b bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
           <div className="flex justify-between items-center">
             <Link href="#" className="flex items-center space-x-2">
-              <MyLogo/>
+              <BriefcaseLogo/>
               <span className="text-xl font-semibold">Jobpilot</span>
             </Link>
             <div className="flex items-center space-x-4">
